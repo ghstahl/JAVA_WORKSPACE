87e56fc764d1eacf9da48ea503b85cda47046801
Herb <ghstahl@gmail.com>
2015-10-20 09:13:19 -0700

    sort implemented and passes unit test.

--- BubbleSort/src/bubbleSort/BubbleSort.java
+++ BubbleSort/src/bubbleSort/BubbleSort.java
@@ -4,9 +4,27 @@ import java.util.List;
 
 public class BubbleSort {
 
-	public static void sort(List<Integer> unsortedList) {
+	public static int sort(List<Integer> unsortedList) {
 		// TODO Auto-generated method stub
 
+		int cost = 0;
+		int count = unsortedList.size();
+		Boolean dirtyList = true;
+		while (dirtyList) {
+			dirtyList = false;
+			for (int i = 0; i < count - 1; ++i) {
+				++cost;
+				int current = unsortedList.get(i);
+				int next = unsortedList.get(i + 1);
+				if (current > next) {
+					dirtyList = true;
+					// swap it.
+					unsortedList.set(i, next);
+					unsortedList.set(i + 1, current);
+				}
+			}
+		}
+		return cost;
 	}
 
 }
