6eea9c253cc708d6bbf9c3a912593d688c95988c
ghstahl@gmail.com <ghstahl@gmail.com>
2015-10-21 06:48:31 -0700

    Initial setup for InsertionSort with unit test and unimplemented sort

---
+++ Sorting/src/sorting/InsertionSort.java
@@ -0,0 +1,7 @@
+package sorting;
+
+import java.util.List;
+
+public class InsertionSort {
+	public static int sort(List<Integer> unsortedList) {return 0;}
+}
---
+++ Sorting/src/sorting/InsertionSortTest.java
@@ -0,0 +1,29 @@
+package sorting;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+import utils.Utils;
+
+public class InsertionSortTest {
+
+	@Test
+	public void testSort() {
+
+		List<Integer> unsortedList = Utils.generateRandomList(1000);
+		List<Integer> sortedList = new ArrayList<Integer>(unsortedList);
+		// use already built sorter to test against.
+		Collections.sort(sortedList);
+
+		int cost = InsertionSort.sort(unsortedList);
+		assertEquals(sortedList, unsortedList);
+		System.out.println("Cost:" + cost);
+
+	}
+
+}
