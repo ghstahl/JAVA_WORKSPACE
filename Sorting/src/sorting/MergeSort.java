3d3c8a2de05cdcb4df5d5f94432cac580814fd86
Herb <ghstahl@gmail.com>
2015-10-21 12:53:42 -0700

    Initial MergeSort with unit test and not implemented sort method

---
+++ Sorting/src/sorting/MergeSort.java
@@ -0,0 +1,14 @@
+package sorting;
+
+import java.util.List;
+
+public class MergeSort {
+
+	public static int sort(List<Integer> unsortedList) {
+		if (unsortedList.size() < 2)
+			return 1;
+		int cost = 0;
+
+		return cost;
+	}
+}
