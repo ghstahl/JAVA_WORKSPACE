7a8a1044198c8b0cbd4350ee3ea0dab43933dd3b
Herb <ghstahl@gmail.com>
2015-10-22 15:12:04 -0700

    Implemented QuickSort with passing unit test.

--- Sorting/src/sorting/QuickSort.java
+++ Sorting/src/sorting/QuickSort.java
@@ -8,8 +8,34 @@ public class QuickSort {
 		// 1 item... done.
 		if (unsortedList.size() < 2)
 			return 1;
-
 		int cost = 0;
+		Integer pivot = unsortedList.get(0);
+		int flipIndex = 1;
+		for (int i = flipIndex; i < unsortedList.size(); ++i) {
+			++cost;
+			Integer flip = unsortedList.get(i);
+			if (flip < pivot) {
+				// swap this with the flip;
+				Integer firstFlip = unsortedList.get(flipIndex);
+				unsortedList.set(flipIndex, flip);
+				unsortedList.set(i, firstFlip);
+				++flipIndex;
+			}
+		}
+
+		Integer lastSmallestItem = unsortedList.get(flipIndex - 1);
+		unsortedList.set(flipIndex - 1, pivot);
+		unsortedList.set(0, lastSmallestItem);
+
+		if (flipIndex > 1) {
+			List<Integer> leftPartion = unsortedList.subList(0, flipIndex - 1);
+			cost += sort(leftPartion);
+		}
+
+		if (flipIndex < (unsortedList.size() - 1)) {
+			List<Integer> rightPartion = unsortedList.subList(flipIndex, unsortedList.size());
+			cost += sort(rightPartion);
+		}
 
 		return cost;
 	}
