b54f2617c21c2e9ca5df4ace8c41632053da5be6
Herb <ghstahl@gmail.com>
2015-10-22 14:01:38 -0700

    initial quicksort TDD unit test with failing implementation.

---
+++ Sorting/src/sorting/QuickSort.java
@@ -0,0 +1,16 @@
+package sorting;
+
+import java.util.List;
+
+public class QuickSort {
+	public static int sort(List<Integer> unsortedList) {
+
+		// 1 item... done.
+		if (unsortedList.size() < 2)
+			return 1;
+
+		int cost = 0;
+
+		return cost;
+	}
+}
