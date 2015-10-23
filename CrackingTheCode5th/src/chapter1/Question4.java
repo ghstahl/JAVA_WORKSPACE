eb1b33ae1996e5fc22291e90558e2ba6ca7b93d6
Herb <ghstahl@gmail.com>
2015-10-23 15:35:15 -0700

    Chapter1.Q4 	/** 	 * Write a method to replace all spaces in a string with'%20'. You may 	 * assume that the string has sufficient space at the end of the string to 	 * hold the additional characters, and that you are given the "true" length 	 * of the string. (Note: if implementing in Java, please use a character 	 * array so that you can perform this operation in place.) EXAMPLE Input: 	 * "Mr John Smith Output: "Mr%20Dohn%20Smith" 	 */
    unimplemented with failing unit tests.

---
+++ CrackingTheCode5th/src/chapter1/Question4.java
@@ -0,0 +1,18 @@
+package chapter1;
+
+public class Question4 {
+	/**
+	 * Write a method to replace all spaces in a string with'%20'. You may
+	 * assume that the string has sufficient space at the end of the string to
+	 * hold the additional characters, and that you are given the "true" length
+	 * of the string. (Note: if implementing in Java, please use a character
+	 * array so that you can perform this operation in place.) EXAMPLE Input:
+	 * "Mr John Smith Output: "Mr%20Dohn%20Smith"
+	 */
+
+	public static String replace(String original, String string, String string2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ CrackingTheCode5th/src/chapter1/Question4Test.java
@@ -0,0 +1,17 @@
+package chapter1;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Question4Test {
+
+	@Test
+	public void testReplace() {
+		String original = "Mr John Smith    ";
+		String outputExpected = "Mr%20John%20Smith";
+		String actualOutput = Question4.replace(original, " ", "%20");
+		assertEquals(outputExpected, actualOutput);
+	}
+
+}
