715dc94a9fac992258d96fe1ea1256b6cef74def
Herb <ghstahl@gmail.com>
2015-10-23 13:06:33 -0700

    implemented and working against unit tests.

--- CrackingTheCode5th/src/chapter1/Question2.java
+++ CrackingTheCode5th/src/chapter1/Question2.java
@@ -6,8 +6,26 @@ public class Question2 {
 	 * reverses a null terminated string
 	 */
 
+	// run time N/2 or just O(N)
 	public static String Reverse(String value) {
-		return null;
+		if (value == null)
+			return null;
+
+		char[] myChars = value.toCharArray();
+
+		int length = value.length();
+		for (int i = 0, r = length - 1; i < length; ++i, --r) {
+			if (i < r) {
+				char left = value.charAt(i);
+				char right = value.charAt(r);
+				myChars[i] = right;
+				myChars[r] = left;
+			} else {
+				break;
+			}
+
+		}
+		return String.valueOf(myChars);
 	}
 
 }
