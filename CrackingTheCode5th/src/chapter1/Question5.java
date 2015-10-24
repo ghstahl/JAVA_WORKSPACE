c83dac2ba8057b0b849d54db6be11a791a40b049
ghstahl@gmail.com <ghstahl@gmail.com>
2015-10-24 07:07:13 -0700

    /** 	 * Implement a method to perform basic string compression using the counts 	 * of repeated characters. For example, the string aabcccccaaa would become 	 * a2blc5a3. If the "compressed" string would not become smaller than the 	 * original string, your method should return the original string. 	 */
    stub implementation with failing unit tests

---
+++ CrackingTheCode5th/src/chapter1/Question5.java
@@ -0,0 +1,17 @@
+package chapter1;
+
+public class Question5 {
+	/**
+	 * Implement a method to perform basic string compression using the counts
+	 * of repeated characters. For example, the string aabcccccaaa would become
+	 * a2blc5a3. If the "compressed" string would not become smaller than the
+	 * original string, your method should return the original string.
+	 */
+
+	public static String compress(String original) {
+
+		return null;
+
+	}
+
+}
---
+++ CrackingTheCode5th/src/chapter1/Question5Test.java
@@ -0,0 +1,29 @@
+package chapter1;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Question5Test {
+
+	@Test
+	public void testCompress_YES() {
+		String original = "aabcccccaaa";
+		String expectedResult = "a2blc5a3";
+
+		String actualResult = Question5.compress(original);
+		assertEquals(expectedResult, actualResult);
+
+	}
+
+	@Test
+	public void testCompress_CompressedBigger() {
+		String original = "abcd";
+		String expectedResult = "abcd";
+
+		String actualResult = Question5.compress(original);
+		assertEquals(expectedResult, actualResult);
+
+	}
+
+}
