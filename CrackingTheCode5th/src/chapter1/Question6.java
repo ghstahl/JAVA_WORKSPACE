a96ae438bce594f3d65f165fec6dc006bc4de278
ghstahl@gmail.com <ghstahl@gmail.com>
2015-10-25 12:08:50 -0700

    Implemented rotate and added another unit test. Even and Odd Grids tested.

--- CrackingTheCode5th/src/chapter1/Question6.java
+++ CrackingTheCode5th/src/chapter1/Question6.java
@@ -10,6 +10,25 @@ public class Question6 {
 	// Running time O(N), everything else (Memory, etc) is constant.
 	public static void Rotate90(ImageContainer imageContainer) {
 
+		int length = imageContainer.matrix[0].length;
+
+		for (int xS = 0; xS < length / 2; ++xS) {
+			int xb = length - 1 - xS;
+			int yb = length - 1 - xS;
+
+			for (int x = xS, y = xS, offset = 0; x < xb; ++x, ++y, ++offset) {
+				int a = imageContainer.matrix[x][y - offset];
+				int b = imageContainer.matrix[xb][y];
+				int c = imageContainer.matrix[xb - offset][yb];
+				int d = imageContainer.matrix[x - offset][yb - offset];
+
+				imageContainer.matrix[x][y - offset] = d;
+				imageContainer.matrix[xb][y] = a;
+				imageContainer.matrix[xb - offset][yb] = b;
+				imageContainer.matrix[x - offset][yb - offset] = c;
+			}
+		}
+
 	}
 
 }
--- CrackingTheCode5th/src/chapter1/Question6Test.java
+++ CrackingTheCode5th/src/chapter1/Question6Test.java
@@ -1,13 +1,13 @@
 package chapter1;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
 
 public class Question6Test {
 
 	@Test
-	public void testRotate90() {
+	public void testRotate90_EvenGrid() {
 
 		/*
 		 * {1,2,3,4},{2,3,4,5},{3,4,5,6},{4,5,6,7}
@@ -22,13 +22,41 @@ public class Question6Test {
 		 * {4,3,2,1},{5,4,3,2},{6,5,4,3},{7,6,5,4}
 		 */
 		ImageContainer expectedImage = new ImageContainer(4);
-		expectedImage.matrix[0] = new int[] { 4, 3, 2, 1 };
-		expectedImage.matrix[1] = new int[] { 5, 4, 3, 2 };
-		expectedImage.matrix[2] = new int[] { 6, 5, 4, 3 };
-		expectedImage.matrix[3] = new int[] { 7, 6, 5, 4 };
+		expectedImage.matrix[0] = new int[] { 4, 5, 6, 7 };
+		expectedImage.matrix[1] = new int[] { 3, 4, 5, 6 };
+		expectedImage.matrix[2] = new int[] { 2, 3, 4, 5 };
+		expectedImage.matrix[3] = new int[] { 1, 2, 3, 4 };
 
+		Question6.Rotate90(image);
 		for (int i = 0; i < 4; ++i) {
-			assertEquals(image.matrix[i], expectedImage.matrix[i]);
+			int[] one = image.matrix[i];
+			int[] two = expectedImage.matrix[i];
+			assertArrayEquals(one, two);
+		}
+
+	}
+
+	@Test
+	public void testRotate90_OddGrid() {
+
+		ImageContainer image = new ImageContainer(5);
+		image.matrix[0] = new int[] { 1, 2, 3, 4, 5 };
+		image.matrix[1] = new int[] { 2, 3, 4, 5, 6 };
+		image.matrix[2] = new int[] { 3, 4, 5, 6, 7 };
+		image.matrix[3] = new int[] { 4, 5, 6, 7, 8 };
+		image.matrix[4] = new int[] { 5, 6, 7, 8, 9 };
+
+		ImageContainer expectedImage = new ImageContainer(5);
+		expectedImage.matrix[0] = new int[] { 5, 6, 7, 8, 9 };
+		expectedImage.matrix[1] = new int[] { 4, 5, 6, 7, 8 };
+		expectedImage.matrix[2] = new int[] { 3, 4, 5, 6, 7 };
+		expectedImage.matrix[3] = new int[] { 2, 3, 4, 5, 6 };
+		expectedImage.matrix[4] = new int[] { 1, 2, 3, 4, 5 };
+		Question6.Rotate90(image);
+		for (int i = 0; i < 5; ++i) {
+			int[] one = image.matrix[i];
+			int[] two = expectedImage.matrix[i];
+			assertArrayEquals(one, two);
 		}
 
 	}
