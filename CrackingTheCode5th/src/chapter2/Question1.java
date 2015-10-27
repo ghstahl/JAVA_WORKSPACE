47b830a4770bf78ab9a039d7fdc9f1e36f930906
ghstahl@gmail.com <ghstahl@gmail.com>
2015-10-27 08:10:22 -0700

    implemented Quadratic removal
    nested while loops
    
    N*(N/2)  or (N^2)/2  or just 0(N^2)

--- CrackingTheCode5th/src/chapter2/Question1.java
+++ CrackingTheCode5th/src/chapter2/Question1.java
@@ -10,7 +10,22 @@ package chapter2;
  */
 public class Question1 {
 
+	// Quadratic removal O(N^2)
 	public static <T> void RemoveDuplicates(LinkedNode<T> head) {
 
+		LinkedNode current = head;
+		while (current != null) {
+			LinkedNode compCurrent = current.Next;
+			while (compCurrent != null) {
+				if (compCurrent.get() == current.get()) {
+					LinkedNode prev = compCurrent.Prev;
+					compCurrent.delete();
+					compCurrent = prev.Next;
+				} else {
+					compCurrent = compCurrent.Next;
+				}
+			}
+			current = current.Next;
+		}
 	}
 }
