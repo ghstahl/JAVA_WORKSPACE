396a4ecc0b6f437943fbb1de737d8c74f235fa7c
Herb <ghstahl@gmail.com>
2015-10-27 13:14:07 -0700

    Implement an algorithm to delete a node in the middle of a singly linked list, given only access to that node. EXAMPLE Input: the node c from the linked list a->b->c->d->e Result: nothing is returned, but the new linked list looks like a- >b- >d->e
    Implemented unit test with stub

---
+++ CrackingTheCode5th/src/chapter2/Question3.java
@@ -0,0 +1,18 @@
+package chapter2;
+
+/**
+ * 
+ * @author Dev
+ * 
+ *         2.3 Implement an algorithm to delete a node in the middle of a singly
+ *         linked list, given only access to that node. EXAMPLE Input: the node
+ *         c from the linked list a->b->c->d->e Result: nothing is returned, but
+ *         the new linked list looks like a- >b- >d->e pg
+ */
+public class Question3 {
+
+	public static void phanthomDelete(LinkedNode head) {
+
+	}
+
+}
