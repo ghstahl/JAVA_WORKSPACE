a3b5b82771eeef1f6bd7545faa37bd20966d08bb
Herb <ghstahl@gmail.com>
2015-10-27 12:47:01 -0700

    /**  *  * @author Dev  *  *         Implement an algorithm to find the kth to last element of a singly  *         linked list.  */
    Unit test with failing stub

---
+++ CrackingTheCode5th/src/chapter2/Question2.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+/**
+ * 
+ * @author Dev
+ * 
+ *         Implement an algorithm to find the kth to last element of a singly
+ *         linked list.
+ */
+public class Question2 {
+
+	public static LinkedNode FetchNthFromEndNode(LinkedNode head, int nth) {
+		return null;
+	}
+
+}
---
+++ CrackingTheCode5th/src/chapter2/Question2Test.java
@@ -0,0 +1,39 @@
+package chapter2;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Question2Test {
+
+	@Test
+	public void testFetchNthFromEndNode() {
+		LinkedNode node0 = new LinkedNode<Integer>(0);
+		LinkedNode node1 = new LinkedNode<Integer>(1);
+		LinkedNode node2 = new LinkedNode<Integer>(2);
+		LinkedNode node3 = new LinkedNode<Integer>(3);
+		LinkedNode node4 = new LinkedNode<Integer>(4);
+
+		node1.prepend(node0);
+		node1.append(node2);
+		node2.append(node4);
+		node4.prepend(node3);
+
+		assertEquals(null, node0.Prev);
+		assertEquals(null, node4.Next);
+
+		assertEquals(node0, node1.Prev);
+		assertEquals(node1, node2.Prev);
+		assertEquals(node2, node3.Prev);
+		assertEquals(node3, node4.Prev);
+
+		assertEquals(node1, node0.Next);
+		assertEquals(node2, node1.Next);
+		assertEquals(node3, node2.Next);
+		assertEquals(node4, node3.Next);
+
+		LinkedNode nthNode = Question2.FetchNthFromEndNode(node0, 4);
+		assertEquals(node1, nthNode);
+	}
+
+}
