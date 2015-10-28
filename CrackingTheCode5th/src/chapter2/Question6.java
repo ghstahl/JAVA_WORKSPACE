f37703151a3bcaa40c6cf236dc390915b9ebdabd
Herb <ghstahl@gmail.com>
2015-10-28 10:27:33 -0700

    implemented find the entry node

--- CrackingTheCode5th/src/chapter2/Question6.java
+++ CrackingTheCode5th/src/chapter2/Question6.java
@@ -14,10 +14,27 @@ package chapter2;
  */
 public class Question6 {
 
+	// this is a 0(N) where we at worst cased have 2N movement throw the list.
 	public static LinkedNode FindBeginingOfLoop(LinkedNode head) {
 
-		return null;
+		LinkedNode slowCurrent = head;
+		LinkedNode fastCurrent = head;
+		// we will do a fast 2x runner which will go until it matches where the
+		// slow runner is.
+		do {
+			slowCurrent = slowCurrent.Next;
+			fastCurrent = fastCurrent.Next.Next;
+		} while (slowCurrent != fastCurrent);
 
+		// collision point.
+
+		slowCurrent = head;
+
+		do {
+			slowCurrent = slowCurrent.Next;
+			fastCurrent = fastCurrent.Next;
+		} while (slowCurrent != fastCurrent);
+		return slowCurrent;
 	}
 
 }
