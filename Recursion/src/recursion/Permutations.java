c2d4bd723eea92b24eb961d4351d6bb7b0970171
ghstahl@gmail.com <ghstahl@gmail.com>
2015-10-31 08:50:11 -0700

    implemented recursive permutation of a string.
    passes 3 unit tests.
    
    Length 1, Length 2, and length 3.

--- Recursion/src/recursion/Permutations.java
+++ Recursion/src/recursion/Permutations.java
@@ -1,12 +1,46 @@
 package recursion;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Permutations {
 
 	public static List<String> makePermutations(String original) {
+		List<String> container = new ArrayList<String>();
+		makePermutations(container, "", original);
 		// TODO Auto-generated method stub
-		return null;
+		return container;
+	}
+
+	/*
+	 * BASE CASE: The front part of the string is passed in. If there is only
+	 * one char in the end then we combine the 2 and add the final string to the
+	 * list.
+	 * 
+	 * Iteration. 1. Front part is passed in, don't touch that. 2. Iterate
+	 * through each character in the back and append that to the front, making a
+	 * new front. 3. Combine the remaining chars in the back and this becomes
+	 * the new back. 4. Pass this new stuff into the recursive method.
+	 * 
+	 */
+	public static void makePermutations(List<String> container, String front, String back) {
+		if (back.length() == 0)
+			return;
+		if (back.length() == 1) {
+			// final string.
+			String perm = front + back;
+			container.add(perm);
+		} else {
+			int length = back.length();
+			for (int i = 0; i < back.length(); ++i) {
+				String newFront = front + back.charAt(i);
+				String newBack = "" + back.substring(0, i);
+				if (i + 1 < length) {
+					newBack += back.substring(i + 1, length);
+				}
+				makePermutations(container, newFront, newBack);
+			}
+		}
 	}
 
 }
--- Recursion/src/recursion/PermutationsTest.java
+++ Recursion/src/recursion/PermutationsTest.java
@@ -10,7 +10,21 @@ import org.junit.Test;
 public class PermutationsTest {
 
 	@Test
-	public void testString() {
+	public void testString_Lenth1() {
+		String original = "e";
+		List<String> perms = Permutations.makePermutations(original);
+
+		List<String> expected = new ArrayList<String>();
+		expected.add("e");
+
+		assertEquals(perms.size(), expected.size());
+		for (int i = 0; i < perms.size(); ++i) {
+			assertEquals(perms.get(i), expected.get(i));
+		}
+	}
+
+	@Test
+	public void testString_Lenth2() {
 		String original = "es";
 		List<String> perms = Permutations.makePermutations(original);
 
@@ -22,7 +36,24 @@ public class PermutationsTest {
 		for (int i = 0; i < perms.size(); ++i) {
 			assertEquals(perms.get(i), expected.get(i));
 		}
-
 	}
 
+	@Test
+	public void testString_Lenth3() {
+		String original = "yes";
+		List<String> perms = Permutations.makePermutations(original);
+
+		List<String> expected = new ArrayList<String>();
+		expected.add("yes");
+		expected.add("yse");
+		expected.add("eys");
+		expected.add("esy");
+		expected.add("sye");
+		expected.add("sey");
+
+		assertEquals(perms.size(), expected.size());
+		for (int i = 0; i < perms.size(); ++i) {
+			assertEquals(perms.get(i), expected.get(i));
+		}
+	}
 }
