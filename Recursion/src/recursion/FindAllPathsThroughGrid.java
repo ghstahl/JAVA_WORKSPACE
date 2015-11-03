645e1112241e61ac1bb075577ff4ed9a65283b12
Herb <ghstahl@gmail.com>
2015-11-03 13:42:14 -0800

    made the find path through grid more object oriented. Preparing for a percolation run.
    http://introcs.cs.princeton.edu/java/24percolation/

--- Recursion/src/recursion/FindAllPathsThroughGrid.java
+++ Recursion/src/recursion/FindAllPathsThroughGrid.java
@@ -5,6 +5,22 @@ import java.util.List;
 
 public class FindAllPathsThroughGrid {
 
+	private boolean _findAll = true;
+	private List<List<Cordinates>> _cords = new ArrayList<List<Cordinates>>();
+	private int[][] _board = null;
+
+	private Cordinates _constraintCord = null;
+
+	public boolean foundAtLeastOne() {
+		return _cords.size() > 0;
+	}
+
+	public FindAllPathsThroughGrid(boolean findAll, int[][] board) {
+		this._findAll = findAll;
+		this._board = board;
+		_constraintCord = new Cordinates(_board.length - 1, _board[0].length - 1);
+	}
+
 	public static List<Cordinates> makeCopy(List<Cordinates> original, int beginIndex) {
 		List<Cordinates> clone = new ArrayList<Cordinates>();
 		for (int i = beginIndex; i < original.size(); ++i) {
@@ -13,42 +29,43 @@ public class FindAllPathsThroughGrid {
 		return clone;
 	}
 
-	public static List<List<Cordinates>> findPathsFromTopToBottomDownAndRightOnly(int[][] board) {
-		Cordinates constraintCord = new Cordinates(board.length - 1, board[0].length - 1);
-		List<List<Cordinates>> coords = new ArrayList<List<Cordinates>>();
+	public static List<List<Cordinates>> findPathsToBottom(boolean findAll, int[][] board) {
 
 		// make a virtual starting point.
 		Cordinates entryCord = new Cordinates(0, -1);
 		Cordinates previousCord = new Cordinates(-1, -1);
-		RecursiveTopToBottomDownAndRightOnly(board, constraintCord, coords, new ArrayList<Cordinates>(), entryCord,
-				previousCord);
-		return coords;
+		FindAllPathsThroughGrid fap = new FindAllPathsThroughGrid(findAll, board);
+
+		fap.RecursiveTopToBottomDownAndRightOnly(new ArrayList<Cordinates>(), entryCord, previousCord);
+		return fap.get_cords();
 	}
 
-	public static void RecursiveTopToBottomDownAndRightOnly(int[][] board, Cordinates constraintCord,
-			List<List<Cordinates>> containerOfSuccess, List<Cordinates> containerOfCurrentPath, Cordinates currentCord,
+	private void RecursiveTopToBottomDownAndRightOnly(List<Cordinates> containerOfCurrentPath, Cordinates currentCord,
 			Cordinates previousCord) {
+
+		if (_findAll == false && foundAtLeastOne()) {
+			return;// bail out and just let it unwind
+		}
 
 		// base case.
 		containerOfCurrentPath.add(currentCord);
-		if (currentCord.y == constraintCord.y) {
+		if (currentCord.y == _constraintCord.y) {
 			// we be at the bottom.
-			containerOfSuccess.add(makeCopy(containerOfCurrentPath, 0));
+			_cords.add(makeCopy(containerOfCurrentPath, 0));
 		} else {
 			// we go right or left, so get the next cords
-			if (currentCord.x < constraintCord.x
-					&& (currentCord.y == -1 || board[currentCord.x + 1][currentCord.y] == 1)) {
+			if (currentCord.x < _constraintCord.x
+					&& (currentCord.y == -1 || _board[currentCord.x + 1][currentCord.y] == 1)) {
 				Cordinates newX = new Cordinates(currentCord.x + 1, currentCord.y);
 
 				if (!newX.equals(previousCord)) {
-					RecursiveTopToBottomDownAndRightOnly(board, constraintCord, containerOfSuccess,
-							containerOfCurrentPath, newX, currentCord);
+					RecursiveTopToBottomDownAndRightOnly(containerOfCurrentPath, newX, currentCord);
 
 				}
 			}
 
 			// since bottoming out on y is covered we just keep going
-			if (board[currentCord.x][currentCord.y + 1] == 1) {
+			if (_board[currentCord.x][currentCord.y + 1] == 1) {
 				// got a good x.
 				Cordinates newX = new Cordinates(currentCord.x, currentCord.y + 1);
 				// this is to show we already have been here, so no more right
@@ -57,22 +74,19 @@ public class FindAllPathsThroughGrid {
 					// set start fresh as we are going down for the first
 					// time.
 					List<Cordinates> internalCurrentPath = new ArrayList<Cordinates>();
-					RecursiveTopToBottomDownAndRightOnly(board, constraintCord, containerOfSuccess, internalCurrentPath,
-							newX, currentCord);
+					RecursiveTopToBottomDownAndRightOnly(internalCurrentPath, newX, currentCord);
 				} else {
-					RecursiveTopToBottomDownAndRightOnly(board, constraintCord, containerOfSuccess,
-							containerOfCurrentPath, newX, currentCord);
+					RecursiveTopToBottomDownAndRightOnly(containerOfCurrentPath, newX, currentCord);
 				}
 
 			}
 			// now see if we need to go left.
-			if (currentCord.y > -1 && currentCord.x > 0 && board[currentCord.x - 1][currentCord.y] == 1) {
+			if (currentCord.y > -1 && currentCord.x > 0 && _board[currentCord.x - 1][currentCord.y] == 1) {
 				// this is to show we already have been here, so no more right
 				// traversal
 				Cordinates newX = new Cordinates(currentCord.x - 1, currentCord.y);
 				if (!newX.equals(previousCord)) {
-					RecursiveTopToBottomDownAndRightOnly(board, constraintCord, containerOfSuccess,
-							containerOfCurrentPath, newX, currentCord);
+					RecursiveTopToBottomDownAndRightOnly(containerOfCurrentPath, newX, currentCord);
 				}
 			}
 		}
@@ -81,6 +95,10 @@ public class FindAllPathsThroughGrid {
 			containerOfCurrentPath.remove(containerOfCurrentPath.size() - 1);
 		}
 
+	}
+
+	public List<List<Cordinates>> get_cords() {
+		return _cords;
 	}
 
 }
--- Recursion/src/recursion/FindAllPathsThroughGridTest.java
+++ Recursion/src/recursion/FindAllPathsThroughGridTest.java
@@ -67,11 +67,74 @@ public class FindAllPathsThroughGridTest {
 			}
 		}
 
-		List<List<Cordinates>> actual = FindAllPathsThroughGrid.findPathsFromTopToBottomDownAndRightOnly(board);
+		List<List<Cordinates>> actual = FindAllPathsThroughGrid.findPathsToBottom(true, board);
 		assertEquals(actual.size(), expected.size());
 		for (int i = 0; i < expected.size(); ++i) {
 			assertEquals(expected.get(i), actual.get(i));
 		}
 	}
 
+	@Test
+	public void test_findAtLeastOne() {
+
+		int board[][] = new int[8][8];
+
+		List<List<Cordinates>> expected = new ArrayList<List<Cordinates>>();
+		List<Cordinates> path = new ArrayList<Cordinates>();
+		path.add(new Cordinates(1, 0));
+		path.add(new Cordinates(1, 1));
+		path.add(new Cordinates(2, 1));
+		path.add(new Cordinates(2, 2));
+		path.add(new Cordinates(3, 2));
+		path.add(new Cordinates(3, 3));
+		path.add(new Cordinates(4, 3));
+		path.add(new Cordinates(5, 3));
+		path.add(new Cordinates(6, 3));
+		path.add(new Cordinates(7, 3));
+		path.add(new Cordinates(7, 4));
+		path.add(new Cordinates(7, 5));
+		path.add(new Cordinates(7, 6));
+		path.add(new Cordinates(7, 7));
+		expected.add(path);
+
+		path = new ArrayList<Cordinates>();
+		path.add(new Cordinates(1, 0));
+		path.add(new Cordinates(1, 1));
+		path.add(new Cordinates(2, 1));
+		path.add(new Cordinates(2, 2));
+		path.add(new Cordinates(3, 2));
+		path.add(new Cordinates(3, 3));
+		path.add(new Cordinates(3, 4));
+		path.add(new Cordinates(3, 5));
+		path.add(new Cordinates(3, 6));
+		path.add(new Cordinates(3, 7));
+		expected.add(path);
+
+		path = new ArrayList<Cordinates>();
+		path.add(new Cordinates(1, 0));
+		path.add(new Cordinates(1, 1));
+		path.add(new Cordinates(2, 1));
+		path.add(new Cordinates(2, 2));
+		path.add(new Cordinates(3, 2));
+		path.add(new Cordinates(3, 3));
+		path.add(new Cordinates(3, 4));
+		path.add(new Cordinates(2, 4));
+		path.add(new Cordinates(1, 4));
+		path.add(new Cordinates(0, 4));
+		path.add(new Cordinates(0, 5));
+		path.add(new Cordinates(0, 6));
+		path.add(new Cordinates(0, 7));
+		expected.add(path);
+
+		for (int i = 0; i < expected.size(); ++i) {
+			for (Cordinates cord : expected.get(i)) {
+				board[cord.x][cord.y] = 1;
+			}
+		}
+
+		List<List<Cordinates>> actual = FindAllPathsThroughGrid.findPathsToBottom(false, board);
+		assertEquals(actual.size(), 1);
+		assertEquals(expected.get(0), actual.get(0));
+
+	}
 }
